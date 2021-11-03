850ade12a62a9526645fe6389ea82237436b62d2
Jacopofede89 <jacopofede89@gmail.com>
2021-11-03 11:42:22 +0100

    refacturing

--- js/script.js
+++ js/script.js
@@ -3,50 +3,44 @@
 // difficoltà 1 => tra 1 e 100
 // difficoltà 2 => tra 1 e 81
 // difficoltà 3 => tra 1 e 49
-// Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
+// Quando l’utente clicca su ogni cella, la cella cliccata si colora di rosso.
+
+const gridCont = document.getElementById("grid");
+// Far scegliere all'utente la difficoltà 
+const livello = prompt("Scegli la difficoltà tra: facile, medio, difficile")
+
+let dimension;
+if(livello == "facile") {
+    dimension = 100
+    choose = "facile"
+} else if( livello == "medio"){
+    dimension = 81
+    choose = "medio"
+} else if (livello== "difficile"){
+    dimension = 49;
+    choose ="difficile";
+}
 
 
-let cointainerEl = document.getElementById('container');
-
-let difficoltaUser;
-// Chiedre a quale difficoltà si vuole giocare
-difficoltaUser = prompt ('Scegli la difficoltà da 1 a 3');
-
-
-//a seconda della scelta dell'utente assegniamo una grandezza numerica alla griglia
-// let box 
-
-//generiamo la  griglia di 100 caselle con un ciclo
-
-if (cointainerEl === "1"){
-    for (let i = 0; i < 100; i++){ 
-        let box = document.getElementById("container")
-    creazione.innerHTML += `
-        <div class="casella"></div>
-    `
+for(let i = 0; i < dimension; i++) {
+    let newElement = elementGen("div", "square", choose);
     
-    }
-//generiamo la  griglia di 81 caselle con un ciclo
-}else if(cointainerEl === "2"){
-    for (let i = 0; i < 81; i++){
-        let box = document.getElementById("container");
-        box.className = "container2";
-        box.innerHTML += `
-            <div class="casella_2"></div>
-        `
-    }
-//generiamo la  griglia di 49 caselle con un ciclo
-}else {
-    for (let i = 0; i < 49; i++){
-        let box = document.getElementById("container");
-        box.className = "container3 ";
-        box.innerHTML += `
-            <div class="casella_3"></div>
-        `
-    }
-};
+    newElement.addEventListener("click",
+    function(){
+        this.classList.add("active")
+        newElement.innerHTML += i+1
+        }
+    )
 
-box.addEventListener('click', 
-function () {
-        this.style.backgroundColor = 'red'
-})
+    gridCont.appendChild(newElement)
+}
+
+
+// funzioni utili
+
+function elementGen(elementGen, classPlus, classP_2) {
+    let node = document.createElement(elementGen);
+    node.classList.add(classPlus)
+    node.classList.add(classP_2)
+    return node
+}
